02abc897e585849dabcdaecf21ba860ed090deaf
joaorocha <joaoeduardorrocha@gmail.com>
2024-05-02 20:24:34 -0300

    feat: create footer component and yours styles

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.scss";
+
+const Footer = () => {
+
+  return (
+    <footer className='footer-container'>
+      <p className='footer-container__rights'>MKS sistemas © Todos os direitos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
