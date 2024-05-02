99a5f9575b2d5f87423dae03f2f21463644af176
joaorocha <joaoeduardorrocha@gmail.com>
2024-05-02 20:24:22 -0300

    feat: create card component and yours styles

---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./Card.scss";
+import svgBag from '../../Assets/shoppingBag.svg';
+import { Product } from '../../Models/Product';
+
+interface CardProps {
+  product: Product;
+  addToCart: (product: Product) => void;
+}
+
+const Card: React.FC<CardProps> = ({ product, addToCart }) => {
+  const handleAddToCart = () => {
+    addToCart(product)
+  };
+
+  return (
+    <article className='card-content'>
+      <div className='card-content__info'>
+        <img className='card-content__img' src={product.photo} alt="Produto" />
+
+        <div className='card-content__name-price-container'>
+          <p className='card-content__name'>{product.name}</p>
+          <p className='card-content__price'>{Number(product.price).toFixed(2)}</p>
+        </div>
+
+        <div className='card-content__description'>
+          {product.description}
+        </div>
+      </div>
+
+      <button className='card-content__btn-buy' onClick={handleAddToCart}>
+        <img src={svgBag} alt="" />
+        COMPRAR
+      </button>
+    </article>
+  );
+};
+
+export default Card;
