6f8b487935881e237e577967073d0610cb3f8c58
joaorocha <joaoeduardorrocha@gmail.com>
2024-05-02 20:24:05 -0300

    feat: create component article and yours styles

---
+++ src/Components/Article/Article.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import "./Article.scss";
+import Card from '../Card/Card';
+import { Product } from '../../Models/Product';
+
+interface ArticleProps {
+  addToCart: (product: Product) => void;
+}
+const Article: React.FC<ArticleProps> = ({ addToCart}) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC');
+      const data = await response.json();
+      setProducts(data.products);
+    } catch (error) {
+      console.error('Ocorreu um erro ao obter os dados dos produtos:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  
+  return (
+    <article className='article-content'>
+      {products.map(product => (
+        <Card key={product.id} product={product} addToCart={addToCart}/>
+      ))}
+    </article>
+  );
+};
+
+export default Article;
